be5de30f2196aa43930a648fb3bb5fe672fa7456
MARYRIVERO <marilyndlvalle892286@gmail.com>
2020-06-03 12:10:53 -0500

    Instala Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+//Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6eGf-YOXK0U1qs3S_IAgpcG-WuitCkMg",
+    authDomain: "crud-products2020.firebaseapp.com",
+    databaseURL: "https://crud-products2020.firebaseio.com",
+    projectId: "crud-products2020",
+    storageBucket: "crud-products2020.appspot.com",
+    messagingSenderId: "656711100750",
+    appId: "1:656711100750:web:f57b8b70aced4c3c79961f"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export {firebase}
