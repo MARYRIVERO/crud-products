ad2212966b582b809ceffa9c11bebfad9b9a3889
MARYRIVERO <marilyndlvalle892286@gmail.com>
2020-06-04 16:47:08 -0500

    Updates

---
+++ precache-manifest.fb755974b7ba4b4c39e0f621e2d1bc12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dc94e7c568e3faa5d888155fde1b4c",
+    "url": "/crud-products/index.html"
+  },
+  {
+    "revision": "ac4f669e7e91f42f5042",
+    "url": "/crud-products/static/css/main.314f0b6e.chunk.css"
+  },
+  {
+    "revision": "c1ae23957b74d35ed5ec",
+    "url": "/crud-products/static/js/2.913d42a3.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/crud-products/static/js/2.913d42a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4f669e7e91f42f5042",
+    "url": "/crud-products/static/js/main.48569f7b.chunk.js"
+  },
+  {
+    "revision": "d73d8866b3635b2be87d",
+    "url": "/crud-products/static/js/runtime-main.5e061587.js"
+  },
+  {
+    "revision": "8e121841db6a3542c852a0d088af4793",
+    "url": "/crud-products/static/media/fondo-tecnologico2.8e121841.svg"
+  }
+]);
